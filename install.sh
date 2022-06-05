ce10c0f3ad5641635cd1c57b4e1b6bc54d20893b
Crazy-Gunix <93563216+Crazy-Gunix@users.noreply.github.com>
2022-06-05 12:57:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-# Setup configs
+# Setup zsh and i3
+
+sudo pacman -Sy
+
+sudo pacman -S --noconfirm zsh-theme-powerlevel10k zsh-syntax-highlighting zsh-history-substring-search zsh-autosuggestions
+
+wget https://github.com/Crazy-Gunix/dotfiles/raw/main/install.sh
